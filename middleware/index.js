ba834c92ef8f6d614bbdd811e7b655fe157c5ced
densuz <densuz97@gmail.com>
2020-04-26 15:59:48 +0700

    #20 Menambahkan URL Menu verifikasi dan secret di index.js

--- middleware/index.js
+++ middleware/index.js
@@ -2,9 +2,13 @@
 var express = require('express');
 var auth = require('./auth');
 var router = express.Router();
+var verifikasi = require('./verifikasi');
 
 //daftarkan menu registrasi
 router.post('/api/v1/register', auth.registrasi);
 router.post('/api/v1/login', auth.login);
+
+//alamat yang perlu otoritasi
+router.get('/api/v1/rahasia', verifikasi(), auth.halamanrahasia)
 
 module.exports = router;
