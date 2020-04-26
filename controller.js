d11c60b03274f179f308cdc3aed8ba3a220708f2
densuz <densuz97@gmail.com>
2020-04-26 12:02:24 +0700

    #11 Berhasil Menampilkan Data Sparepart

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
     response.ok("Aplikasi Rest API WORKING!", res)
 };
+
+//menampilkan data sparepart dan montir
+exports.tampildatasparepart = function(req,res){
+    connection.query('SELECT * FROM t_sparepart', function(error, rows, fields){
+        if(error){
+            console.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
